4f342326cd1b1da3b4d7cbad69dad1426d51fe10
datsuns <the.skeleton7@gmail.com>
2024-08-11 21:27:43 +0900

    set cursor style to whole are of OnlineStatus

--- src/lib/TwitchChannelStatus/TwitchChannelStatus.svelte
+++ src/lib/TwitchChannelStatus/TwitchChannelStatus.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
-  import Card, { PrimaryAction } from '@smui/card';
+  import Card from '@smui/card';
 
   const dispatch = createEventDispatcher();
 
@@ -14,20 +14,16 @@
 </script>
 
 {#if online}
-  <Card style="color: #444444; background-color: #5AFF19; align-items: center;">
-    <PrimaryAction on:click={handleClick} >
-      {name}
-      <br>
-      ({channel})
-    </PrimaryAction>
+  <Card style="color: #444444; background-color: #5AFF19; align-items: center; cursor: pointer;" on:click={handleClick}>
+    {name}
+    <br>
+    ({channel})
   </Card>
 {:else}
-  <Card style="color: #FFFFFF; background-color: #444444; align-items: center;">
-    <PrimaryAction on:click={handleClick} >
-      {name}
-      <br>
-      ({channel})
-    </PrimaryAction>
+  <Card style="color: #FFFFFF; background-color: #444444; align-items: center; cursor: pointer;" on:click={handleClick}>
+    {name}
+    <br>
+    ({channel})
   </Card>
 {/if}
 
